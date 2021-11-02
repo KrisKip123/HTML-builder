f91d7683557802940319a74b1975e29eb141afa0
KrisKip123 <kirill.lipnitskii@mail.ru>
2021-11-02 22:18:55 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs').promises;
+const path = require('path');
+const f = require('fs');
 
+let fullNamePath = path.resolve(__dirname, 'files');
+let copyFullNamePath = path.resolve(__dirname, 'files-copy');
+
+
+
+async function cloneFolder(){
+    await fs.rmdir(copyFullNamePath, {recursive:true});
+    await fs.mkdir(copyFullNamePath,{recursive:true});
+    let files = await fs.readdir(fullNamePath, {
+        withFileTypes: true,
+    });
+    let filesFilter = files.filter(e => e.isFile());
+    for (let i = 0; i < filesFilter.length; i++) {
+        let backPath = await path.resolve(fullNamePath, filesFilter[i].name);
+        let newPath = await path.resolve(copyFullNamePath, filesFilter[i].name);
+        let backStream = await f.createReadStream(backPath, 'utf8');
+        let newStream = await f.createWriteStream(newPath);
+        await backStream.pipe(newStream);
+    }
+}
+
+cloneFolder();
