9ee8cf9a575ebb6d2a6cc3eb63f7a887af42f41d
KrisKip123 <kirill.lipnitskii@mail.ru>
2021-11-03 17:38:27 +0300

    fix: image

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,7 +6,6 @@ let fullNamePath = path.resolve(__dirname, 'files');
 let copyFullNamePath = path.resolve(__dirname, 'files-copy');
 
 async function cloneFolder() {
-  await fs.rmdir(copyFullNamePath, { recursive: true });
   await fs.mkdir(copyFullNamePath, { recursive: true });
   let files = await fs.readdir(fullNamePath, {
     withFileTypes: true,
@@ -15,7 +14,12 @@ async function cloneFolder() {
   for (let i = 0; i < filesFilter.length; i++) {
     let backPath = await path.resolve(fullNamePath, filesFilter[i].name);
     let newPath = await path.resolve(copyFullNamePath, filesFilter[i].name);
-    let backStream = await f.createReadStream(backPath, 'utf8');
+    let backStream;
+    if(filesFilter[i].name.slice(-3) === 'jpg'){
+      backStream = await f.createReadStream(backPath);
+    }else{
+      backStream = await f.createReadStream(backPath, 'utf8');
+    }
     let newStream = await f.createWriteStream(newPath);
     await backStream.pipe(newStream);
   }
